8d8a9f074fbb922468be5442b53ed00159d28ec1
VarshiniManokaran <varshini6678@gmail.com>
2024-08-24 16:16:09 +0530

    Add files via upload

---
+++ Scanners.java
@@ -0,0 +1,22 @@
+   import java.util.Scanner;
+public class Scanners {
+    public static void main (String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Name");
+        String name=sc.nextLine();
+        System.out.println("Enter your Age");
+        int addresse=sc.nextInt();
+        System.out.println("Enter your Address");
+        int city=sc.nextInt();
+        System.out.println("Enter your city");
+        String respect=sc.nextLine();
+        System.out.println("Thank You");
+        sc.close();
+
+       
+
+    
+    }
+
+    
+}
