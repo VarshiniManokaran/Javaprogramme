d05f3d520245c898cfecbbc35cdfd030aebaa4ea
VarshiniManokaran <varshini6678@gmail.com>
2024-08-26 22:10:37 +0530

    Add files via upload

---
+++ instance.java
@@ -0,0 +1,9 @@
+public class instance {
+    public static void main(String[] args) {
+
+        instance ins=new instance();
+        System.out.println(ins instanceof instance);
+        
+    }
+    
+}
